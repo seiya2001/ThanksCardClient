aa83fb3038e5cb53ca31689c59c9d1dfd5a310b0
seiya2001 <seiya-t@occ.co.jp>
2020-06-01 14:14:55 +0900

    add-comment-for-departmentclass

--- ThanksCardClient/Models/Department.cs
+++ ThanksCardClient/Models/Department.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ThanksCardClient.Services;
-
+// Department クラス
 namespace ThanksCardClient.Models
 {
     public class Department : BindableBase
