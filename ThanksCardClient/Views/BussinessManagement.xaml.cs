44593921470b5c4a8b66d75e5b33cbc24946671f
Reishin-Sakuma <reisin@occ.co.jp>
2020-06-04 13:26:43 +0900

    経営画面ログインのファイル名変更＆オブジェクト位置最適化

---
+++ ThanksCardClient/Views/BussinessManagement.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ThanksCardClient.Views
+{
+    /// <summary>
+    /// Interaction logic for BussinessManagement
+    /// </summary>
+    public partial class BussinessManagement : UserControl
+    {
+        public BussinessManagement()
+        {
+            InitializeComponent();
+        }
+    }
+}
