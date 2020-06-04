3ccf160f69a7a751bcc0a590ccf750d1c3405777
Reishin-Sakuma <reisin@occ.co.jp>
2020-06-04 11:11:54 +0900

    ログイン画面の最終調整済

--- ThanksCardClient/ViewModels/LogonViewModel.cs
+++ ThanksCardClient/ViewModels/LogonViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Commands;
+﻿using Newtonsoft.Json;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
 using System;
@@ -31,6 +32,8 @@ namespace ThanksCardClient.ViewModels
             set { SetProperty(ref _ErrorMessage, value); }
         }
         #endregion
+
+        
 
         public LogonViewModel(IRegionManager regionManager)
         {
