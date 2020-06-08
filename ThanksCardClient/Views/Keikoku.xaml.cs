46078aaea1075727cacfb8904b2e70b28ad556a9
seiya2001 <seiya-t@occ.co.jp>
2020-06-08 10:26:42 +0900

    送信警告画面

---
+++ ThanksCardClient/Views/Keikoku.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ThanksCardClient.Views
+{
+    /// <summary>
+    /// Interaction logic for Keikoku
+    /// </summary>
+    public partial class Keikoku : UserControl
+    {
+        public Keikoku()
+        {
+            InitializeComponent();
+        }
+    }
+}
